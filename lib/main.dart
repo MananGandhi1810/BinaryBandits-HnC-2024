367306721d3ca59d39b42e0dad653504506930d6
Manan Gandhi <ardumanan@gmail.com>
2024-04-07 01:36:29 +0530

    Language done

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,26 @@ import 'package:binary_bandits_hnc/main_page.dart';
 import 'package:binary_bandits_hnc/verfiy.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'login.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await dotenv.load(fileName: ".env");
   runApp(
     MaterialApp(
       initialRoute: 'main',
-
-      theme: ThemeData(fontFamily: 'Jura', brightness: Brightness.dark,),
+      theme: ThemeData(
+        fontFamily: 'Jura',
+        brightness: Brightness.dark,
+      ),
       debugShowCheckedModeBanner: false,
       routes: {
-        'phone': (context) => MyPhone(),
-        'verify': (context) => MyVerify(),
-        'main': (context) => main_page(),
+        'phone': (context) => const MyPhone(),
+        'verify': (context) => const MyVerify(),
+        'main': (context) => const main_page(),
       },
     ),
   );
